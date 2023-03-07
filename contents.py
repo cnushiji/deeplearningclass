33149964cdfaed0a7e826fa35b293cf2ea6d4e00
ShiJi <shiji@cnu.edu.cn>
2023-03-07 22:31:12 +0800

    Update contents.py

--- contents.py
+++ contents.py
@@ -7,15 +7,13 @@ st.markdown(
   <body>
   <h1></h1>
   <h1></h1>
+  <h1 font="15"><center>深度学习的数学基础与工程实践</center></h1>
   <h1></h1>
   <h1></h1>
-  <h1 font="20">
-  <center>深度学习的数学基础与工程实践</center>
-  </h1>
   <body>
   """,unsafe_allow_html=True)
 
-st.markdown(' ## 目录')
+st.markdown('### 目录')
 
 st.write("[Chapter1 深度学习概览]()")
 st.write("[Chapter2 深度前馈神经网络]()")
