cc42c468b2c0edf0e328d47d00aea0dfee6d0f7b
shiji666 <shiji@cnu.edu.cn>
2023-03-07 20:04:52 +0800

    v1

--- Contents.py
+++ Contents.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write(""" # 深度学习的数学基础与工程实践目录 """)
+st.markdown(' 深度学习的数学基础与工程实践')
+
+st.title('目录')
+
+st.write("Chapter1 深度学习概览 [link]()")
+st.write("Chapter2 深度前馈神经网络 [link]()")
+st.write("Chapter3 卷积神经网络 [link]()")
+st.write("Chapter4 自然语言处理[link]()")
+st.write("Chapter5 生成模型 [link]()")
+st.write("Chapter6 强化学习和迁移学习 [link]()")
