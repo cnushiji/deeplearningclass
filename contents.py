105183fd03e986a857e575529d998314dd74a115
ShiJi <shiji@cnu.edu.cn>
2023-03-07 22:28:13 +0800

    Update contents.py

--- contents.py
+++ contents.py
@@ -1,12 +1,25 @@
 import streamlit as st
+import streamlit.components.v1 as cp
+import base64
 
-st.markdown('<center># 深度学习的数学基础与工程实践</center>')
+st.markdown(
+  """
+  <body>
+  <h1></h1>
+  <h1></h1>
+  <h1></h1>
+  <h1></h1>
+  <h1 font="20">
+  <center>深度学习的数学基础与工程实践</center>
+  </h1>
+  <body>
+  """,, unsafe_allow_html=True)
 
-st.title(' ## 目录')
+st.markdown(' ## 目录')
 
 st.write("[Chapter1 深度学习概览]()")
-st.write("Chapter2 深度前馈神经网络 [link]()")
-st.write("Chapter3 卷积神经网络 [link]()")
-st.write("Chapter4 自然语言处理[link]()")
-st.write("Chapter5 生成模型 [link]()")
-st.write("Chapter6 强化学习和迁移学习 [link]()")
+st.write("[Chapter2 深度前馈神经网络]()")
+st.write("[Chapter3 卷积神经网络]()")
+st.write("[Chapter4 自然语言处理]()")
+st.write("[Chapter5 生成模型]()")
+st.write("[Chapter6 强化学习和迁移学习]()")
