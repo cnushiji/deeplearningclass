9c9ccfde046e641ad6dde78cd111c638d8a13003
ShiJi <shiji@cnu.edu.cn>
2023-03-08 11:02:16 +0800

    Update contents.py

--- contents.py
+++ contents.py
@@ -16,8 +16,8 @@ st.markdown(
 st.markdown('### 目录')
 
 st.write("[Chapter1 深度学习概览](https://shiji666-deeplearningc-chapter1-introductionpresentation-dzvjs0.streamlit.app/)")
-st.write("[Chapter2 深度前馈神经网络]()")
-st.write("[Chapter3 卷积神经网络]()")
-st.write("[Chapter4 自然语言处理]()")
-st.write("[Chapter5 生成模型]()")
-st.write("[Chapter6 强化学习和迁移学习]()")
+st.write("[Chapter2 深度前馈神经网络DFNN](https://shiji666-deeplearningclass-chapter2-dfnnpresentation-bpav9d.streamlit.app/)")
+st.write("[Chapter3 卷积神经网络CNN](https://shiji666-deeplearningclass-chapter3-cnnpresentation-vq91qx.streamlit.app/)")
+st.write("[Chapter4 自然语言处理NLP](https://shiji666-deeplearningclass-chapter4-nlppresentation-ycycwr.streamlit.app/)")
+st.write("[Chapter5 生成模型GM](https://shiji666-deeplearningclass-chapter5-gmpresentation-msfd0d.streamlit.app/)")
+st.write("[Chapter6 迁移学习TL和强化学习RL](https://shiji666-deeplearningclass-chapter6-tlrlpresentation-qgjjsi.streamlit.app/)")
